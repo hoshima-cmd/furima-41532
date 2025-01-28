c250f5f8e533632ebb688d8e27be4dff2eac000b
hoshima-cmd <hmhmdrm0815@gmail.com>
2025-01-29 00:44:58 +0900

    ユーザー管理-1

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+    validates :Last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX 
+
+
+  has_many :items
+  has_many :comments
+end
+

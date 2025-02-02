d6bffbff13fb2d4b5216cab6fc7583992d2fb73d
hoshima-cmd <hmhmdrm0815@gmail.com>
2025-02-02 14:54:47 +0900

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,7 @@ class UsersController < ApplicationController
 
 private
 
+def user_params
+  params.require(:user).permit(:email, :password)
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: :show
   get 'items/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "items#index"
 end

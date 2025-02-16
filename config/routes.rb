f974629fceb71619deb282fd26b8b64dfcfbfe04
hoshima-cmd <hmhmdrm0815@gmail.com>
2025-02-16 13:48:08 +0900

    pay.jp-1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :orders , only: [:index,:create]
   root to: 'items#index'
 
 end

b7f5bf5a6f01ebb1691ce1d10460d250d03041d6
hoshima-cmd <hmhmdrm0815@gmail.com>
2025-02-17 16:22:58 +0900

    LGMT後の修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -4,15 +4,12 @@ class ItemOrder
                 :region_id, :street_address, :building, :token
 
   with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :prefecture
-    validates :token
-    validates :street_address
+    validates :user_id, :item_id, :prefecture, :token, :street_address
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
+    validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
   end
-  validates :post_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
-  validates :region_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  
   def save
     order = Order.create(user_id:, item_id:)
     Sell.create(order_id: order.id, post_code:, prefecture:, building:, street_address:, region_id:, telephone_number:)
